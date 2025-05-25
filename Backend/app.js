222acdaa18a0668569a62d45257759d175a51f63
unknown <Neha>
2025-05-25 15:31:41 +0530

    worked on product model and controller

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler, notFound } from "./Middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import productRoute from "./routes/product.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -8,8 +13,19 @@ const app = express();
 app.use(express.json());
 
 
+//cookie parser
+app.use(cookieParser());
+
 //cors
 app.use(cors());
+
+//Routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/products", productRoute);
+
+// Error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 
 export default app;
